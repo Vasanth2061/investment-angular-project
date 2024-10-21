11580c7d736b9cfcb9fafa7a2452f7846ed5134f
Vasanth M <vasanthj036@gmail.com>
2024-10-21 13:02:27 +0530

    Created result component

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { type investment } from '../app.model';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+    enteredInvestmentAmount = '10';
+    enteredAnnualInvestment = '10';
+    enteredExpectedReturn = '10';
+    enteredDuration = '5';
 
+    @Output() investmentData = new EventEmitter<investment>();
+
+    onSubmit(){
+      this.investmentData.emit({
+        initialInvestment: +this.enteredInvestmentAmount,
+        annualInvestment: +this.enteredAnnualInvestment,
+        expectedReturn: +this.enteredExpectedReturn,
+        duration: +this.enteredDuration
+      });
+    }
 }
